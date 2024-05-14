0ed2ccd95c7f63daf6c68661aa7a881b16fd58d8
devrisby <118779687+devrisby@users.noreply.github.com>
2024-05-14 14:41:29 -0400

    complete day 2

---
+++ 2024/may/14-python-wordcount/README.md
@@ -0,0 +1,4 @@
+# Word Counter
+
+**May 14, 2024 Tuesday**
+- To practice python, today I will create a program that accepts a txt file and a word, and counts the occurrences of that word
---
+++ 2024/may/14-python-wordcount/main.py
@@ -0,0 +1,41 @@
+# Read text file & "word" arg
+# Parse text file into hashmap, key = "word", value = counter
+# Display "value" for the given word arg
+
+import sys
+import re
+
+def read_file(filename):
+    with open(file=filename, encoding="utf-8") as f:
+        return f.read()
+        
+def clean(text):
+    cleaned_words = re.sub("[^\w\s]", "", text)
+    cleaned_words = re.sub("(\r\n|\r|\n)", " ", cleaned_words)
+    return re.sub("  ", " ", cleaned_words)
+    
+def extract(text):
+    word_counts = {}
+    words = text.split(" ")
+    
+    for word in words:
+        word = word.lower()
+        if word not in word_counts:
+            word_counts[word] = 0
+            
+        word_counts[word] += 1
+        
+    return word_counts
+        
+def main():
+    text = read_file("sample.txt")
+    raw_words = clean(text)
+    words = extract(raw_words)
+    args = sys.argv
+    if len(args) <= 1 or not args[1] or args[1].lower() not in words:
+        print("Word not found")
+        return
+        
+    print("Occurrences:", words[args[1].lower()])
+        
+main()
