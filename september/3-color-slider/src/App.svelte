d2564e13474c6d264a397068cc7fd770d02686b0
devrisby <118779687+devrisby@users.noreply.github.com>
2024-09-04 05:38:32 -0400

    completes day 15

---
+++ september/3-color-slider/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Color from "./Color.svelte";
+  let red = 0, green = 0, blue = 0
+
+</script>
+
+<h1>Color Picker</h1>
+<div class="main">
+  <Color colorName="red" bind:colorValue={red}/>
+  <Color colorName="green" bind:colorValue={green} />
+  <Color colorName="blue" bind:colorValue={blue}/>
+  <div class="container" style="background-color: rgb({red},{green},{blue});"></div>
+  <p class="rgb">rgb({red},{green},{blue})</p>
+</div>
+
+<style>
+  .main {
+    height: 50%;
+    width: 90%;
+  }
+
+  .container {
+    border: 5px dotted black;
+    width: 80vw;
+    height: 40vh;
+    min-height: 1px;
+    margin: 1rem;
+  }
+
+  .rgb {
+    margin: 1rem;
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+</style>
---
+++ september/3-color-slider/src/Color.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let colorName = "red"
+    export let colorValue = 0
+</script>
+
+<div class="container">
+    <label for="color">{colorName} ({colorValue})</label>
+    <input type="range" name="color" id="color" min="0" max="255" bind:value={colorValue}>
+</div>
+
+<style>
+    .container {
+        width: 100%;
+        margin: 2rem 1rem;
+    }
+    .container label {
+        text-transform: capitalize;
+        padding: 6px;
+        font-weight: bold;
+    }
+
+    .container input {
+        display: block;
+        width: 100%;
+    }
+</style>
